b708b160de334252ca67a9a3d729cd6542547792
kedor <99351777+aure31@users.noreply.github.com>
2025-07-23 13:55:44 +0200

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+use pumpkin::plugin::Context;
+use pumpkin_api_macros::{plugin_impl, plugin_method};
+
+#[plugin_method]
+async fn on_load(&mut self, server: &Context) -> Result<(), String> {
+    pumpkin::init_log!();
+    log::info!("Hello, Pumpkin");
+    Ok(())
+}
+
+#[plugin_impl]
+pub struct MyPlugin {}
+
+impl MyPlugin {
+    pub fn new() -> Self {
+        MyPlugin {}
+    }
+}
+impl Default for MyPlugin {
+    fn default() -> Self {
+        Self::new()
+    }
+}
